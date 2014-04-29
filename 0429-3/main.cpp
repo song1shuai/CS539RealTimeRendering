f388730606a6ac4f76a6e14f30ba46eb081d1c67
kiya69 <kiya69@gmail.com>
2014-04-29 15:23:24 -0400

    Initial Commit

---
+++ 0429-3/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  0429-3
+//
+//  Created by kiya on 4/29/14.
+//  Copyright (c) 2014 kiya. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
